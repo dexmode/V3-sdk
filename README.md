1413f7da53323dd01d2b882fa8a9d948016ee5b8
dexmode <163574123+dexmode@users.noreply.github.com>
2024-03-18 00:29:49 +0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,1 @@
+V3 SDK to Build on top of DexMode Interface
---
+++ src/constants.ts
@@ -0,0 +1,25 @@
+export const FACTORY_ADDRESS = '0x6aAF57A3b6f8aD6AB25CAD4412FE7e83378bcd80'
+
+export const ADDRESS_ZERO = '0x0000000000000000000000000000000000000000'
+
+export const POOL_INIT_CODE_HASH = '0xe34f199b19b2b4f47f68442619d555527d244f78a3297ea89325f843f87b8b54'
+
+/**
+ * The default factory enabled fee amounts, denominated in hundredths of bips.
+ */
+export enum FeeAmount {
+  LOWEST = 100,
+  LOW = 500,
+  MEDIUM = 3000,
+  HIGH = 10000
+}
+
+/**
+ * The default factory tick spacings by fee amount.
+ */
+export const TICK_SPACINGS: { [amount in FeeAmount]: number } = {
+  [FeeAmount.LOWEST]: 1,
+  [FeeAmount.LOW]: 10,
+  [FeeAmount.MEDIUM]: 60,
+  [FeeAmount.HIGH]: 200
+}
